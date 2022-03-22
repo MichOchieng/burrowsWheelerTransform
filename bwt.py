1ce33e6858455466dcbd25fc0d3467a47bec11e3
MichOchieng <MichOchieng@gmail.com>
2022-03-22 00:02:40 -0700

    Added more functions

--- bwt.py
+++ bwt.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 class bwt:
 
-    INPUT_FILES = []
+    INPUT_FILES  = []
+    INPUT_STRING: str
 
     def __init__(self) -> None:
         self.getFiles()
@@ -14,8 +15,24 @@ class bwt:
         myPath = Path(__file__).parent.resolve()
         for file in next(walk(myPath), (None, None, []))[2]:
             if re.search("^input_.*.txt",file):
-                print("Found: " +  file)
                 self.INPUT_FILES.append(file)
+
+    def getString(self,file):
+        try:
+            with open(file) as inputFile:
+                lines = inputFile.readlines()
+                self.INPUT_STRING = lines[0]
+        except:
+            print("Error reading input file.")
+
+    def burrowsWheeler(self,string):
+        print(string)
+
+    def run(self):
+        for file in self.INPUT_FILES:
+            self.getString(file)
+            self.burrowsWheeler(self.INPUT_STRING)
 
 if __name__ == "__main__":
     program = bwt()
+    program.run()
