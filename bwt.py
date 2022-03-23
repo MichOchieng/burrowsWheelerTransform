5a97f33247b9caf758f05d7096171ab705619b61
MichOchieng <MichOchieng@gmail.com>
2022-03-23 00:09:18 -0700

    Now expects input string to be a BWT

--- bwt.py
+++ bwt.py
@@ -9,9 +9,7 @@ class bwt:
     INPUT_FILES  = []
     INPUT_STRING: str
 
-    BW_MATRIX  = []
-    FIRST_COL  = ""
-    SECOND_COL = ""
+    SORTED_INPUT: str
 
     RESULT     = ""
     DICTIONARY: dict
@@ -31,54 +29,45 @@ class bwt:
             with open(file) as inputFile:
                 lines = inputFile.readlines()
                 self.INPUT_STRING = lines[0]
+                self.SORTED_INPUT = ''.join(sorted(lines[0]))
+                self.RESULT       = ""
         except:
             print("Error reading input file.")
 
-    def createMatrix(self,string):
-        for x in range(len(string)):
-            string = self.rotate(string)
-            self.BW_MATRIX.append(string)
+    def addCount(self,string) -> str:
+        temp = ""
+        for c in string:
+            temp = temp + (c + (str(temp.count(c))))
+        return temp
 
-    def getTransform(self,string):
-        # Generate + sort matrix
-        self.createMatrix(string)
-        self.BW_MATRIX.sort()
-
-        for x in self.BW_MATRIX:
-            self.FIRST_COL  = (self.FIRST_COL + x[0]) + str(self.FIRST_COL.count(x[0]))
-            self.SECOND_COL = (self.SECOND_COL + x[-1]) + str(self.SECOND_COL.count(x[-1])) 
-
-        self.RESULT = self.RESULT + self.FIRST_COL[:2]
-        # create a dictionary using each letter in FIRST_COL with the corresponding letter in SECOND_COL
+    def invert(self):
+        self.INPUT_STRING = self.addCount(self.INPUT_STRING)
+        self.SORTED_INPUT = self.addCount(self.SORTED_INPUT)
+        
+        # Add $ to the result
+        self.RESULT = self.RESULT + self.SORTED_INPUT[:2]
+        
         self.DICTIONARY = dict(
                     zip(
-                        re.findall("..",self.FIRST_COL),
-                        re.findall("..",self.SECOND_COL)
+                        re.findall("..",self.SORTED_INPUT),
+                        re.findall("..",self.INPUT_STRING)
                         )
                     )
-        
-        self.getInverse(self.FIRST_COL[:2])
+        self.search(self.SORTED_INPUT[:2])
     
-    def getInverse(self,key):
-        # Loop through first transform 
-        if len(self.RESULT) < len(self.FIRST_COL):
+    def search(self,key):
+        if len(self.RESULT) < len(self.SORTED_INPUT):
             char = self.DICTIONARY[key]
             self.RESULT = self.RESULT + char
-            self.getInverse(char)
+            self.search(char)
         else: # Reverse the string
             self.RESULT = self.RESULT[::-1]
-
-    def rotate(sel,string) -> str:
-        # Moves last char in a string to the front of the string
-        char = string[len(string)-1]
-        # print("Rotation: " + (char + string[:-1]))
-        return (char + string[:-1])
 
     def run(self):
         for file in self.INPUT_FILES:
             self.getString(file)
-            self.getTransform(self.INPUT_STRING)
-            print(re.sub(r'\d+','',self.RESULT[:-1]))
+            self.invert()
+            print(file + " produced an output of: " + re.sub(r'\d+','',self.RESULT[:-1]))
 
 if __name__ == "__main__":
     program = bwt()
